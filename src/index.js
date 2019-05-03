3b7557b8ce71eb473eb318639862cad60c5cddf7
Thomas Malt <thomas@malt.no>
2019-05-03 08:02:41 +0200

    rewrote cli frontend to use commander for structure

--- src/index.js
+++ src/index.js
@@ -1,75 +1,76 @@
 #!/usr/bin/env node
-const fetch = require('node-fetch');
 const package = require('../package');
-const config = require('../etc/sbanken');
-const btoa = require('btoa');
+const credentials = require('../etc/sbanken');
 const Sbanken = require('./node-sbanken');
+const program = require('commander');
+const chalk = require('chalk');
 const { name, description, version } = package;
 
-console.log(`${name} - ${description} - Version: ${version}`);
-
 if (process.env.SBANKEN_SECRET) {
-  config.secret = process.env.SBANKEN_SECRET;
+  credentials.secret = process.env.SBANKEN_SECRET;
 }
 
 if (process.env.SBANKEN_CLIENTID) {
-  config.clientId = process.env.SBANKEN_CLIENTID;
+  credentials.clientId = process.env.SBANKEN_CLIENTID;
 }
 
 if (process.env.SBANKEN_USERID) {
-  config.userId = process.env.SBANKEN_USERID;
+  credentials.userId = process.env.SBANKEN_USERID;
 }
 
-sbanken = new Sbanken(config);
+sbanken = new Sbanken(credentials);
+
+program.version(version).description(description);
+program.option('-v, --verbose', 'Tell the program to be verbose');
+
+program
+  .command('accounts')
+  .description('List all accounts')
+  .action(() => {
+    sbanken.options({ verbose: program.verbose });
+    if (program.verbose) {
+      console.info('Command: List all accounts.');
+    }
+    sbanken.accounts().then(json => {
+      console.table(json.items);
+    });
+  });
+
+program
+  .command('account [name]')
+  .description('List accounts with a given name')
+  .action(name => {
+    if (program.verbose) {
+      console.info('Told to list account by name: ' + name);
+    }
+    const regex = new RegExp(name, 'ui');
+    sbanken.accounts().then(json => {
+      json.items
+        .filter(item => item.name.match(regex))
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .forEach(item => {
+          console.log(
+            item.name.padEnd(32),
+            chalk.white.bold(`${item.balance.toFixed(2)} kr`.padStart(12))
+          );
+        });
+    });
+  });
+
+program.parse(process.argv);
+
+program.on('command:*', () => {
+  console.error(
+    'Unknown command: %s\nSee --help for a list of available commands.',
+    program.args.join(' ')
+  );
+  process.exit(1);
+});
+
+// console.log(`${name} - ${description} - Version: ${version}`);
+
 // sbanken.hello();
 
-sbanken.accounts();
-
-//  .getAccessToken()
-//  .then(data => {
-//    console.log('Got token:');
-//    // console.log(data);
-//    return data;
-//  })
-//  .then(data => {
-//    // console.log(data.access_token);
-//    return fetch('https://api.sbanken.no/bank/api/v1/accounts/', {
-//      headers: {
-//        Authorization: `Bearer ${data.access_token}`,
-//        Accept: 'application/json',
-//        customerId: config.userId,
-//      },
-//    });
-//  })
-//  .then(res => {
-//    if (res.ok) {
-//      return res.json();
-//    }
-//
-//    throw new Error(res.status + ' ' + res.statusText);
-//  })
-//  .then(json => {
-//    console.log('Got accounts:');
-//    console.table(
-//      json.items.map(i => ({
-//        accountNumber: i.accountNumber,
-//        name: i.name,
-//        available: i.available
-//          .toFixed(2)
-//          .toString()
-//          .padStart(10, ' '),
-//        balance: i.balance
-//          .toFixed(2)
-//          .toString()
-//          .padStart(10, ' '),
-//      }))
-//    );
-//    // console.log(json);
-//    // json.items.forEach(account => {
-//    //   console.log('%s: kr %f', account.name, account.available);
-//    // });
-//  })
-//  .catch(err => {
-//    console.error('Got Error:', err.message);
-//  });
-//
+// sbanken.accounts().then(json => {
+//   console.table(json.items);
+// });
