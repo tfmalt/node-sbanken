eb0d628477248661dd7cdef5ab10c0c4d1d24689
Thomas Malt <thomas@malt.no>
2020-08-10 15:39:14 +0200

    making npm install work

--- dist/src/log.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    info: (...msgs: (string | number)[]) => void;
-    debug: (...msgs: (string | number)[]) => void;
-    warn: (...msgs: (string | number)[]) => void;
-    error: (...msgs: (string | number)[]) => void;
-    log: (...msgs: (string | number)[]) => void;
-};
-export default _default;
