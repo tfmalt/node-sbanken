1ad97680ee84a3cf467feaa029cd70a5cf29c96a
Thomas Malt <thomas@malt.no>
2020-08-18 12:58:39 +0200

    cleaning up access-token code

--- src/access-token.ts
+++ src/access-token.ts
@@ -1,4 +1,4 @@
-import * as fetch from 'node-fetch';
+// import * as nodeFetch from 'node-fetch';
 import { version } from '../package.json';
 import urls from './sbanken-urls.json';
 import { AccessTokenInfo, Credentials } from './node-sbanken';
@@ -21,13 +21,16 @@ let DEBUG = false;
 
 function isFresh(): boolean {
   const info = ACCESS_TOKEN_DATA.info;
-  DEBUG && console.log('access-token.isFresh: ', JSON.stringify(info));
-  if (typeof info === 'undefined') return false;
-  if (!(info as AccessTokenInfo).access_token) return false;
-  if (!(info as AccessTokenInfo).date) return false;
-  if (info.date !== 'string') info.date = new Date('1971-26-08').toJSON();
+  let infoDate = info.date;
 
-  const then = new Date(info.date);
+  if (typeof info === 'undefined') return false;
+  if (!info.access_token) return false;
+  if (!infoDate) return false;
+  if (infoDate !== 'string') {
+    infoDate = new Date('1971-26-08').toJSON();
+  }
+
+  const then = new Date(infoDate as string);
   const now = new Date();
 
   if (then.getTime() + (info.expires_in - 300) * 1000 > now.getTime()) {
@@ -47,53 +50,59 @@ const access = {
     DEBUG && console.log('Debug output is turned on.');
     return DEBUG;
   },
+
   get: async (creds: Credentials): Promise<AccessTokenData> => {
-    DEBUG && console.log('access-token get');
+    // DEBUG && console.log('access-token DEBUG get', JSON.stringify(creds));
     if (isFresh()) {
       DEBUG && console.log('access-token fresh:', true);
       return ACCESS_TOKEN_DATA;
     }
 
-    const params = new URLSearchParams([['grant_type', 'client_credentials']]);
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
 
     // only load btoa if not defined (available in browsers and web workers)
     const __btoa = typeof btoa === 'undefined' ? require('btoa') : btoa;
-    const auth = __btoa(encodeURIComponent(creds.clientId) + ':' + encodeURIComponent(creds.secret));
+    const __fetch = typeof fetch === 'undefined' ? require('node-fetch') : fetch;
 
-    const init: fetch.RequestInit = {
+    const auth = __btoa(encodeURIComponent(creds.clientId) + ':' + encodeURIComponent(creds.secret));
+    const init = {
       method: 'post',
       body: params,
       headers: {
         Accept: 'application/json',
-        Authorization: `Basic ${auth}`,
+        Authorization: 'Basic ' + auth,
+        'Content-Type': 'application/x-www-form-urlencoded',
         customerId: creds.userId,
       },
     };
 
     DEBUG && console.log('access-token get init:', JSON.stringify(init));
+    DEBUG && console.log('  params:', init.body.toString());
+    DEBUG && console.log('  URL:', urls.auth);
+    DEBUG && console.log('  typeof fetch:', typeof __fetch);
 
-    return fetch
-      .default(urls.auth, init)
-      .then((res: fetch.Response) => {
-        if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-        return res.json();
-      })
-      .then((json) => {
-        json.date = new Date().toJSON();
+    try {
+      const res = await __fetch(urls.auth, init);
+      DEBUG && console.log('access-token fetch:', res.status, res.statusText);
+      // if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+      const json = await res.json();
+      json.date = new Date().toJSON();
 
-        ACCESS_TOKEN_DATA.info = json;
+      ACCESS_TOKEN_DATA.info = json;
 
-        if (DEBUG) {
-          if (typeof ACCESS_TOKEN_DATA.debug === 'undefined') ACCESS_TOKEN_DATA.debug = {};
-          ACCESS_TOKEN_DATA.debug.version = version;
-        }
+      if (DEBUG) {
+        if (typeof ACCESS_TOKEN_DATA.debug === 'undefined') ACCESS_TOKEN_DATA.debug = {};
+        ACCESS_TOKEN_DATA.debug.version = version;
+      }
 
-        DEBUG && console.log('access-token get data: ', ACCESS_TOKEN_DATA);
-        return ACCESS_TOKEN_DATA;
-      })
-      .catch((err) => {
-        throw err;
-      });
+      DEBUG && console.log('access-token get data: ', ACCESS_TOKEN_DATA);
+      return ACCESS_TOKEN_DATA;
+    } catch (e) {
+      console.log('Got error in access token fetch.', e);
+    }
+
+    return ACCESS_TOKEN_DATA;
   },
 };
 
